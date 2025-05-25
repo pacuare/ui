094b9ebaf1a30ee64bb4585d45460c1ba50668b6
Aleks Rūtiņš <keeper@farthergate.com>
2025-05-24 22:48:06 -0400

    Add db creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,12 @@
 	import AuthenticatedHome from "./AuthenticatedHome.svelte";
 	import Loading from "./Loading.svelte";
 	import { fade } from "svelte/transition";
+	import * as db from "$lib/api/db";
+	import { spinner } from "$lib/components/spinner";
+	import { LoaderCircle } from "@lucide/svelte";
 
     let access = $state<AccessInfo>()
+    let creatingDB = $state(false)
 
     onMount(async () => {
         try {
@@ -16,10 +20,24 @@
         } catch (e) {
             goto('/login')
         }
+
+        if(!await db.exists(client)) {
+            creatingDB = true
+            await db.create(client)
+            creatingDB = false
+        }
     })
 </script>
 
-{#if access != undefined}
+{#if creatingDB}
+    <main class="flex flex-col gap-3 p-3 flex-1 w-screen h-screen items-center justify-center">
+        <div {...spinner()}>
+            <LoaderCircle/>
+        </div>
+        <h2 class="font-bold">Welcome to Pacuare Reserve!</h2>
+        <p>Please wait while we set up your account...</p>
+    </main>
+{:else if access != undefined}
 <main class="flex flex-1 w-screen h-screen items-stretch justify-stretch" transition:fade>
     <AuthenticatedHome {access} />
 </main>
