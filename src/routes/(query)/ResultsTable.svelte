b69926aac74d7fe6854216bc17789ef5a27bbba3
Aleks Rūtiņš <keeper@farthergate.com>
2025-05-24 21:58:13 -0400

    Add loading spinner to queries

--- src/routes/(query)/ResultsTable.svelte
+++ src/routes/(query)/ResultsTable.svelte
@@ -1,37 +1,46 @@
 <script lang="ts">
-	import { client } from "$lib/api/client";
-	import { query, type QueryResponse } from "$lib/api/query";
-    import * as card from "$lib/components/card";
-    import Lazy from 'svelte-lazy';
+	import { client } from '$lib/api/client';
+	import { query, type QueryResponse } from '$lib/api/query';
+	import { spinner } from '$lib/components/spinner';
+	import { LoaderCircle } from '@lucide/svelte';
 
-    let results = $state<QueryResponse>()
+	let loading = $state(false);
+	let results = $state<QueryResponse>();
 
-    export async function runQuery(q: string) {
-        results = await query(client, q, [])
-    }
+	export async function runQuery(q: string) {
+		loading = true;
+		results = await query(client, q, []);
+		loading = false;
+	}
 </script>
 
-<table>
-    {#if results}
-        <thead>
-            <tr class="border-b border-gray-300">
-                {#each results.columns as col}
-                    <th class="p-2 bg-gray-100">{col}</th>
-                {/each}
-            </tr>
-        </thead>
-        <tbody>
-            {#each results.values as row}
-                <tr class="[:not(:last-child)]:border-b border-gray-300">
-                    {#each row as cell}
-                        <td class="p-2">{cell}</td>
-                    {/each}
-                </tr>
-            {/each}
-        </tbody>
-    {:else}
-        <tbody>
-            <tr class="h-full flex px-4 py-3 items-center justify-center"><td>Type a semicolon to run your query.</td></tr>
-        </tbody>
-    {/if}
-</table>
+{#if loading}
+	<div class="flex flex-1 h-full items-center justify-center">
+		<div {...spinner()}>
+			<LoaderCircle />
+		</div>
+	</div>
+{:else if results}
+	<table>
+		<thead>
+			<tr class="border-b border-gray-300">
+				{#each results.columns as col}
+					<th class="bg-gray-100 p-2">{col}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each results.values as row}
+				<tr class="border-gray-300 [:not(:last-child)]:border-b">
+					{#each row as cell}
+						<td class="p-2">{cell}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{:else}
+	<div class="flex h-full items-center justify-center px-4 py-3">
+		<p>Type a semicolon to run your query.</p>
+	</div>
+{/if}
