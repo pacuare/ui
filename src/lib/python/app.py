cf5acc245b8ef7d02a3a97ffa260bd14e8dab14b
Aleks Rutins <keeper@farthergate.com>
2025-05-27 16:44:05 -0400

    Don't depend on nonexistent tables for example

--- src/lib/python/app.py
+++ src/lib/python/app.py
@@ -4,10 +4,10 @@ from pacuare import Client
 db = Client()
 
 async def process(id):
-    sql = "select injuries, turtle_occurrences from unique_turtles where turtle_id = %s"
+    sql = "SELECT count(turtle_id) AS turtle_occurrences, string_agg(injuries, ''::text) AS injuries FROM pacuare_raw WHERE turtle_id = %s GROUP BY turtle_id;"
     params = [id]
     if id == '':
-        sql = "select injuries, turtle_occurrences from unique_turtles"
+        sql = "SELECT count(turtle_id) AS turtle_occurrences, string_agg(injuries, ''::text) AS injuries FROM pacuare_raw WHERE (turtle_id <> ''::text) GROUP BY turtle_id;"
         params = []
     return (
         (await db.query(sql, params))
